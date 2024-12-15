a05f6c86139a94d366a4a0401b9bc34a249a8052
hknsh <valterliviaamaral@gmail.com>
2024-12-15 20:51:14 +0000

    refactor: username is now required instead of displayName

--- src/users/dto/update_name.dto.ts
+++ src/users/dto/update_name.dto.ts
@@ -4,16 +4,16 @@ import { z } from "nestjs-zod/z";
 export const UpdateNameSchema = z
 	.object({
 		username: z
-			.string()
+			.string({ required_error: "Username is required" })
 			.regex(
 				/^[a-zA-Z0-9_.]{5,15}$/,
 				"The username must have alphanumerics characters, underscore, dots and it must be between 5 and 15 characters",
 			)
 			.toLowerCase()
-			.describe("New username - optional")
 			.optional()
+			.describe("Old / New Username")
 			.or(z.literal("")),
-		displayName: z.string({ required_error: "Display name is required" }),
+		displayName: z.string().optional().or(z.literal("")),
 	})
 	.required();
 
